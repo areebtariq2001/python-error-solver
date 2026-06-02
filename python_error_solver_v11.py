bc70d5bbf3f6dd609618474d0e505067d1b1f2af
areebtariq2001 <areebtaiq20@gmail.com>
2026-06-02 08:22:11 -0700

    Create python_error_solver_v11.py
    Add V11 Widget - Final Production Version

---
+++ python_error_solver_v11.py
@@ -0,0 +1,11 @@
+import re
+import difflib
+import keyword
+import traceback
+import sys
+import io
+from contextlib import redirect_stdout
+import ipywidgets as widgets
+from IPython.display import display, clear_output, HTML
+...
+widget.run()
